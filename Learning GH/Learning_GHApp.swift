d262a3ae61bfed7a9f0796b9fab6dc26962ca06e
raduliano <radulian@me.com>
2022-03-21 18:39:11 +0200

    Initial Commit

---
+++ Learning GH/Learning_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GHApp.swift
+//  Learning GH
+//
+//  Created by Octav Radulian on 21.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
